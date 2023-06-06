6c853095d994acfdf0448fb4bd4521b51008c6f1
Taislaine Massucato <massucato2@outlook.com>
2023-06-06 16:44:43 -0300

    Finalização da utilização e estudo de Mapeamento usando DataAnnotation

--- Data/EFCoreDataContext.cs
+++ Data/EFCoreDataContext.cs
@@ -11,7 +11,10 @@ namespace EFCore.Data
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-           => options.UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True");
+        {
+            options.UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True");
+            options.LogTo(System.Console.WriteLine);
+        }
         
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,48 +1,78 @@
 ﻿using System;
 using EFCore.Data;
 using EFCore.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFCore
- {
+{
     internal class Program
     {
         static void Main(string[] args)
         {
-          using(var context = new EFCoreDataContext())
-          {
-              var user = new User
-              {
-                  Name = "John Doe",
-                  Email = "johndoe@example.com",
-                  PasswordHash = "hashedpassword",
-                  Bio = "I'm a software developer",
-                  Image = "profile-image.jpg",
-                  Slug = "john-doe"
-              };
+            using (var context = new EFCoreDataContext())
+            {
+                // var user = new User
+                // {
+                //     Name = "John Doe",
+                //     Email = "johndoe@example.com",
+                //     PasswordHash = "hashedpassword",
+                //     Bio = "I'm a software developer",
+                //     Image = "profile-image.jpg",
+                //     Slug = "john-doe"
+                // };
 
-              var categoria = new Category
-              {
-                Name = "Devops",
-                Slug = "Consertar Rede"
-              };
+                // var categoria = new Category
+                // {
+                //   Name = "Devops",
+                //   Slug = "Consertar Rede"
+                // };
 
-              var post = new Post
-              {
-                Category = categoria, 
-                Author = user, 
-                Title = "Sample Post",
-                Summary = "This is a sample post summary",
-                Body = "This is the body of the sample post",
-                Slug = "sample-post",
-                CreateDate = DateTime.Now,
-                LastUpdateDate = DateTime.Now,
-             
-              };
-              context.Posts.Add(post);
-              context.SaveChanges();
-          } 
+                // var post = new Post
+                // {
+                //   Category = categoria, 
+                //   Author = user, 
+                //   Title = "Sample Post",
+                //   Summary = "This is a sample post summary",
+                //   Body = "This is the body of the sample post",
+                //   Slug = "sample-post",
+                //   CreateDate = DateTime.Now,
+                //   LastUpdateDate = DateTime.Now,
+
+                // };
+                // context.Posts.Add(post);
+                // context.SaveChanges();
+
+                                        //INCLUDE
+                // var posts = context.
+                //             Posts.
+                //             AsNoTracking().
+                //             Include(x => x.Author). //tipo innerjoin
+                //             Include(x => x.Category).
+                //             OrderByDescending(x => x.LastUpdateDate).
+                //             ToList();
+
+                // foreach (var post in posts)
+                // {
+                //            System.Console.WriteLine($"{post.Title} escrito por {post.Author?.Name} em {post.Category.Name}"); //Usar ? caso haja possibilidade de nulo
+
+
+                var posts = context.
+                            Posts.
+                            //AsNoTracking().
+                            Include(x => x.Author). 
+                            Include(x => x.Category).
+                            OrderByDescending(x => x.LastUpdateDate).
+                            FirstOrDefault(); //PEGANDO PRIMEIRO ITEM
+
+
+                            posts.Author.Name = "Taislaine";
+
+                            context.Posts.Update(posts);
+                            context.SaveChanges();
+                }
+            }
         }
     }
-  }
+
 
 
